f96603e4ccf7f0f1f8b4571ce5b3ac7d2d89bad4
Madalin Ignisca <git@imadalin.ro>
2014-01-13 13:06:28 +0200

    Course 201, Lesson Conclusion

---
+++ IntroToCs/src/introtocs/inheritanceandpoly/CreditCard.java
@@ -0,0 +1,27 @@
+package introtocs.inheritanceandpoly;
+
+public class CreditCard extends MoneyCard {
+
+	private int creditLimit;
+	
+	public CreditCard(int amount) {
+		this.balance = 0;
+		this.creditLimit = amount;
+	}
+	
+	public void makePayment(int amount) {
+		this.balance = this.balance - amount;
+	}
+	
+	public int getAvailableCredit() {
+		return this.creditLimit - this.balance;
+	}
+	
+	public void makePurchase(int amount) {
+		if (amount > getAvailableCredit()) {
+			throw new IllegalArgumentException("Amount of the purchase exceeds the available credit");
+		}
+		
+		this.balance = this.balance + amount;
+	}
+}
