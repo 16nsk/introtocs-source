410b710e023c4f6ddee2a927a303461765f0a99c
Madalin Ignisca <git@imadalin.ro>
2014-01-14 10:05:38 +0200

    Course 301, Lesson Selection Sort

---
+++ IntroToCs/src/introtocs/arrayalgorithms/FillingAndDeleting.java
@@ -0,0 +1,33 @@
+package introtocs.arrayalgorithms;
+
+import java.util.Arrays;
+
+public class FillingAndDeleting {
+
+	public static void main(String[] args) {
+		// Filling Arrays
+
+		int[] oneThroughTen = new int[10];
+		
+		for (int i = 0; i < oneThroughTen.length; i++) {
+			oneThroughTen[i] = i + 1;
+		}
+		
+		System.out.println(Arrays.toString(oneThroughTen));
+		
+		// Delering Elements
+		
+		String[] names = { "John", "Mary", "Nancy", "Bob" };
+		names[1] = names[names.length - 1];
+		String[] newNames = new String[names.length - 1];
+		
+		for (int i = 0; i < newNames.length; i++) {
+			newNames[i] = names[i];
+		}
+		
+		System.out.println(Arrays.toString(names));
+		System.out.println(Arrays.toString(newNames));
+		
+	}
+
+}
